9b923c0360babd0ac5c7591c0666f84e46c8dc1e
Andreea Mocanu <andreea.oana.mocanu@gmail.com>
2018-09-21 02:19:49 +0300

    Add POST endpoint and use a body parser

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 app.get('/', (req, res) => {
-    console.log("Received " + req.method + ": " + req.body)
+    logRequest(req)
     res.send('Hello Node.js!')
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/', (req, res) => {
+    logRequest(req)
+    res.send('Hello Node.js!')
+})
+
+function logRequest(req) {
+    var body = ((req.method === "POST") ? (JSON.stringify(req.body)) : "")
+    console.log("Received " + req.method + " " + req.url + " " + body)
+}
