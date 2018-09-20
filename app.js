04071eda387cb920850e7b997d8aac9b24c4061b
Andreea Mocanu <andreea.oana.mocanu@gmail.com>
2018-09-20 19:43:37 +0300

    Log all http requests

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello Node.js!'))
+app.get('/', (req, res) => {
+    console.log("Received " + req.method + ": " + req.body)
+    res.send('Hello Node.js!')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
