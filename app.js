23a77f1bf364d4055e315337b2f75ed15b4106a0
Andreea Mocanu <andreea.oana.mocanu@gmail.com>
2018-09-21 12:31:35 +0300

    Reply to user messages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request');
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -15,11 +16,30 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    logRequest(req)
+    logRequest(req)    
+    sendBotReply(req.body.message.from.id, req.body.message.text);
     res.send('Hello Node.js!')
 })
 
 function logRequest(req) {
-    var body = ((req.method === "POST") ? (JSON.stringify(req.body)) : "")
-    console.log("Received " + req.method + " " + req.url + " " + body)
+    var body = ((req.method === "POST") ? (" " + JSON.stringify(req.body)) : "")
+    console.log("Received " + req.method + " " + req.url + body + " from " + req.headers.host)
+}
+
+function sendBotReply(userId, message) {
+    var data = (message.toLowerCase() === 'wassa wassa wassa') ? 'Hey hey hey' : 'Sorry, I don\'t undestand.';
+    var options = {
+        uri: 'https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN + '/sendMessage',
+        body: JSON.stringify({ chat_id: userId, text: data}),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    console.log(options.uri);
+    console.log(options.body);
+    request(options, function (error, response) {
+        console.log(error, response.body);
+        return;
+    });
 }
